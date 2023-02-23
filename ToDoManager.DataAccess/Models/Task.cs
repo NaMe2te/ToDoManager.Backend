746d04edaeb3668d11d9f16f7e1fc722ac49d21a
NaMe2te <vladyushashishkin81@gmail.com>
2023-02-23 20:41:51 +0300

    init

---
+++ ToDoManager.DataAccess/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoManager.DataAccess.Models;
+
+public class Task
+{
+    public Task(int id, string text, DateTime? deadline = null)
+    {
+        Id = id;
+        Text = text;
+        IsCompleted = false;
+        Deadline = deadline;
+    }
+
+    public int Id { get; init; }
+    public string Text { get; init; }
+    public bool IsCompleted { get; private set; }
+    public DateTime? Deadline { get; private set; }
+
+    public DateTime AddDeadline(DateTime deadline)
+    {
+        Deadline = deadline;
+        return deadline;
+    }
+}
